803eb71086d86a8faa2876eb19a5eecfe96fbe04
Michael Ross <michael.ross.contact@gmail.com>
2023-04-16 20:35:50 +0100

    Added buttons and logic to display the selected button

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ function calculator(num1, num2, operator) {
     return result;
   }
   
+// get all numeric buttons
+const numericButtons = document.querySelectorAll('.btn:not(#add-button):not(#subtract-button):not(#multiply-button):not(#divide-button):not(#clear-button):not(#equals-button)');
+
+// add event listener to each numeric button
+numericButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue + button.textContent;
+  });
+});
+
